05c2a1997c8789e5e40753d889a337450c379110
Hirofumi Wakasugi <baenej@gmail.com>
2015-11-23 11:23:09 +0900

    [WIP] implementing...

--- lib/crystal/dash/docset/generator.rb
+++ lib/crystal/dash/docset/generator.rb
@@ -1,11 +1,77 @@
 require "crystal/dash/docset/generator/version"
+require "open-uri"
+require "nokogiri"
+require "fileutils"
 
-module Crystal
-  module Dash
-    module Docset
-      module Generator
-        # Your code goes here...
+module Crystal::Dash::Docset::Generator
+  class Generator
+    attr_accessor :root_url
+    attr_accessor :package_name
+
+    def initialize(root_url: nil, package_name: nil)
+      @root_url = root_url
+      @package_name = package_name
+    end
+
+    def prepare_directory
+      return nil if File.exist?(@package_name)
+      Dir.mkdir(@package_name)
+      Dir.chdir(@package_name)
+    end
+
+    def create_css
+      Dir.mkdir("css")
+      open("css/style.css", "wb") do |output|
+        open("#{root_url}css/style.css") do |f|
+          content = f.read
+          # Remove unwanted styles
+          content.gsub!(/left: 20%;/, "")
+          content.gsub!(/right: 0;/, "")
+          content.gsub!(/position: absolute;/, "")
+          content.gsub!(/overflow: hidden;/, "")
+          output.write(content)
+        end
+      end
+    end
+
+    def set_page_urls
+      charset = nil
+      root_html = open(@root_url) do |f|
+        charset = f.charset
+        f.read
+      end
+      root_doc = Nokogiri::HTML.parse(root_html, nil, charset)
+      types_list = root_doc.css("div#types-list")
+      @page_urls = types_list.css('a').inject([]) do |result, anchor|
+        result << anchor[:href]
+      end
+    end
+
+    def create_source_docs
+      @page_urls.each do |url|
+        puts url
+        charset = nil
+        # puts URI.join(@root_url, url)
+        FileUtils.mkdir_p(File.dirname(url))
+        open(url, "wb") do |output|
+          html = open(URI.join(@root_url, url)) do |f|
+            charset = f.charset
+            f.read
+          end
+          doc = Nokogiri::HTML.parse(html, nil, charset)
+          doc.css("div#types-list").remove
+          output.write(doc.to_html)
+        end
       end
     end
   end
 end
+
+@generator = Crystal::Dash::Docset::Generator::Generator.new(
+  root_url: "http://crystal-lang.org/api/",
+  package_name: "crystal"
+)
+@generator.prepare_directory
+@generator.create_css
+@generator.set_page_urls
+@generator.create_source_docs
--- test/crystal/dash/docset/generator_test.rb
+++ test/crystal/dash/docset/generator_test.rb
@@ -1,11 +1,65 @@
 require 'test_helper'
+require 'fileutils'
 
 class Crystal::Dash::Docset::GeneratorTest < Minitest::Test
+  i_suck_and_my_tests_are_order_dependent!
+
+  def setup
+    @generator = Crystal::Dash::Docset::Generator::Generator.new(
+      root_url: "http://crystal-lang.org/api/",
+      package_name: "crystal"
+    )
+    Dir.chdir(File.join(File.dirname(__FILE__), "test_data"))
+    if File.exist?(@generator.package_name)
+      FileUtils.rm_rf(@generator.package_name)
+    end
+  end
+
+  def teardown
+    if File.exist?(@generator.package_name)
+      FileUtils.rm_rf(@generator.package_name)
+    end
+  end
+
   def test_that_it_has_a_version_number
     refute_nil ::Crystal::Dash::Docset::Generator::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_it_has_root_url
+    assert_equal @generator.root_url, "http://crystal-lang.org/api/"
+  end
+
+  def test_it_has_package_name
+    assert_equal @generator.package_name, "crystal"
+  end
+
+  def test_prepare_directory_returns_nil_if_already_exist
+    Dir.mkdir(@generator.package_name)
+    assert_nil @generator.prepare_directory
+  end
+
+  def test_prepare_directory_works
+    assert_equal 0, @generator.prepare_directory
+    assert_equal File.join(File.dirname(__FILE__), "test_data", @generator.package_name), Dir.getwd
+    Dir.chdir(File.join(File.dirname(__FILE__), "test_data"))
+    assert File.directory?(@generator.package_name)
+  end
+
+  def test_create_css
+    @generator.prepare_directory
+    @generator.create_css
+    assert File.file?(File.join(File.dirname(__FILE__), "test_data", @generator.package_name, "css", "style.css"))
+  end
+
+  def test_set_page_urls
+    urls = @generator.set_page_urls
+    assert_equal ["index.html", "toplevel.html"], urls[0..1]
+  end
+
+  def test_create_source_docs
+    @generator.prepare_directory
+    @generator.create_css
+    @generator.set_page_urls
+    @generator.create_source_docs
   end
 end
